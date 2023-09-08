4ad155a88a97abb506d3efd52da9ce51e8106bfa
Priyesh123 <priyesh.pandey@bitcs.in>
2023-09-08 14:58:53 +0530

    added local storage feature

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const itemList = document.getElementById("itemList");
 
   submitButton.addEventListener("click", function () {
-
     const amount = expenseAmountInput.value;
     const description = expenseDescriptionInput.value;
     const category = categoryDropdown.value;
 
     const listItem = createListItem(amount, description, category);
     itemList.appendChild(listItem);
+
+    saveInLocalStorage(amount, description, category);
 
     expenseAmountInput.value = "";
     expenseDescriptionInput.value = "";
@@ -29,24 +30,52 @@ document.addEventListener("DOMContentLoaded", function () {
     const editButton = listItem.querySelector(".edit");
     const deleteButton = listItem.querySelector(".delete");
 
-
     editButton.addEventListener("click", function () {
-        if(expenseAmountInput.value != '' || expenseDescriptionInput.value != ''){
-            alert('cannot update since you already have an entry filled')
-            return;
-        }
-        itemList.removeChild(listItem);
-        expenseAmountInput.value = amount;
-        expenseDescriptionInput.value = description;
-        categoryDropdown.value = category;
-        console.log("Edit button clicked for:", amount, description, category);
+      if (
+        expenseAmountInput.value != "" ||
+        expenseDescriptionInput.value != ""
+      ) {
+        alert("cannot update since you already have an entry filled");
+        return;
+      }
+      itemList.removeChild(listItem);
+      deleteFromLocalStorage(amount,description,category);
+      expenseAmountInput.value = amount;
+      expenseDescriptionInput.value = description;
+      categoryDropdown.value = category;
+      console.log("Edit button clicked for:", amount, description, category);
     });
-
 
     deleteButton.addEventListener("click", function () {
-        itemList.removeChild(listItem);
-    });
+      itemList.removeChild(listItem);
 
+      deleteFromLocalStorage(amount,description,category);
+    });
     return listItem;
-}
+  }
+
+  function saveInLocalStorage(amount, description, category) {
+    const data = {
+      Expense: amount,
+      Description: description,
+      Category: category,
+    };
+
+    const lsItemList = JSON.parse(localStorage.getItem("expenses")) || [];
+    lsItemList.push(data);
+    localStorage.setItem("expenses", JSON.stringify(lsItemList));
+  }
+
+  function deleteFromLocalStorage(amount, description, category) {
+    const lsItemList = JSON.parse(localStorage.getItem('expenses')) || [];
+    const index = lsItemList.findIndex(item => (
+      item.Expense === amount &&
+      item.Description === description
+    ));
+
+    if (index !== -1) {
+        lsItemList.splice(index, 1);
+        localStorage.setItem('expenses', JSON.stringify(lsItemList));
+      }
+  }
 });
